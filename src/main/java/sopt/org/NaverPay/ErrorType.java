bab3377a5318693737bcf5f939fb9ded155862aa
eeddiinn <gdw4013@naver.com>
2023-11-21 02:44:01 +0900

    [CHORE] 성공, 예외 ENUM 세팅

---
+++ src/main/java/sopt/org/NaverPay/ErrorType.java
@@ -0,0 +1,13 @@
+package sopt.org.NaverPay;
+
+import org.springframework.http.HttpStatus;
+
+public enum ErrorType {
+
+    /**
+     * 400 BAD REQUEST
+     */
+
+    // Common
+    REQUEST_VALIDATION_EXCEPTION(HttpStatus.BAD_REQUEST, "잘못된 요청입니다"),
+}
---
+++ src/main/java/sopt/org/NaverPay/SuccessType.java
@@ -0,0 +1,11 @@
+package sopt.org.NaverPay;
+
+import org.springframework.http.HttpStatus;
+
+public enum SuccessType {
+
+    /**
+     * 200 OK
+     */
+    LOGIN_SUCCESS(HttpStatus.OK, "로그인에 성공했습니다."),
+}
