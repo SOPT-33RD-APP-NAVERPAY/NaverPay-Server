e93767b8a9884f366d387acf7834bda69a3a19f1
eeddiinn <gdw4013@naver.com>
2023-11-21 01:26:26 +0900

    [CHORE] 프로젝트 초기 #1

---
+++ src/main/java/sopt/org/NaverPay/NaverPayApplication.java
@@ -0,0 +1,13 @@
+package sopt.org.NaverPay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaverPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaverPayApplication.class, args);
+	}
+
+}
