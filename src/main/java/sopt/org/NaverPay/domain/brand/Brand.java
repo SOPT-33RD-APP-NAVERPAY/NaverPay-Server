4655b6fd13800805bc4a5ca39bdc9092b9671b2a
jun02160 <jun020216@naver.com>
2023-11-21 18:29:05 +0900

    [FEAT] User, Brand 엔티티 클래스 추가 #4

---
+++ src/main/java/sopt/org/NaverPay/domain/brand/Brand.java
@@ -0,0 +1,43 @@
+package sopt.org.NaverPay.domain.brand;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class Brand {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "brand_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String logoImgUrl;
+
+    @Column(nullable = false)
+    private String discountContent;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private DiscountType discountType;
+
+    private String place;
+
+    private int distance;
+}
